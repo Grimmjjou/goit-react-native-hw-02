e8cc4026012287dfcda4723c832fc08c568987dc
Grimmjjou <grimmjjou@gmail.com>
2023-05-30 18:04:25 +0300

    01

---
+++ goit-react-native-hw-02/App.js
@@ -0,0 +1,28 @@
+import LoginScreens from "./Screens/LoginScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from '@react-navigation/stack';
+import RegistrationScreen from "./Screens/RegistrationScreen";
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Registration"
+          component={RegistrationScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Log in"
+          component={LoginScreens}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
